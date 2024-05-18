07003fd2b7b3edca747967162fe7de22bb1fd925
Dhanasekar <dhanasekarn0409@gmail.com>
2024-05-18 17:25:25 +0530

    Implement sent data to backend

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,51 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddUser = () => {
+  const navigate = useNavigate("/");
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const { name, username, email } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post("http://localhost:8080/user", user);
+    navigate("/");
+  };
+
   return (
     <>
       <div className="container text-center">
         <div className="row">
           <div className="col-xl-6 offset-xl-3 border rounded p-4 mt-2 shadow">
             <h2 className="m-4">Register User</h2>
-            <div className="mb-3">
-              <label htmlFor="Name" className="col-form-label fw-bold ms-1">
-                Name
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your name"
-                name="name"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="username" className="col-form-label fw-bold ms-1">
-                Username
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your username"
-                name="username"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="email" className="col-form-label fw-bold ms-1">
-                Email
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your email"
-                name="email"
-              />
-            </div>
-            <button type="submit" className="btn btn-outline-primary">
-              Submit
-            </button>
-            <button type="submit" className="btn btn-outline-danger mx-2">
-              Cancel
-            </button>
+            <form onSubmit={(e) => onSubmit(e)}>
+              <div className="mb-3">
+                <label htmlFor="Name" className="col-form-label fw-bold ms-1">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your name"
+                  name="name"
+                  value={name}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="username" className="col-form-label fw-bold ms-1">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your username"
+                  name="username"
+                  value={username}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="col-form-label fw-bold ms-1">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+              <button type="submit" className="btn btn-outline-primary">
+                Submit
+              </button>
+              <button type="submit" className="btn btn-outline-danger mx-2">
+                Cancel
+              </button>
+            </form>
           </div>
         </div>
       </div>
